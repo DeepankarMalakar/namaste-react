35cfdcdf35649534272b0f1cb57d081e4d59bcff
DeepankarMalakar <deepankarmalakar007@gmail.com>
2024-08-19 21:56:09 +0530

    Updated files

--- Ep-3-Laying-the-foundation/App3.js
+++ Ep-3-Laying-the-foundation/App3.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { createElement } from 'react';
 import ReactDOM from 'react-dom/client';
 
+// JSX - JSX is not HTML inside JS, JSX is HTML-like or XML-like syntax.
+// JSX is syntax extension for javascript.
 
-const heading = React.createElement("h1", { id: "heading", xyz: "abc" }, "Hello World from React!");
-console.log(heading);    // returns an object
+const jsxHeading = (<h1 id='heading' className='head'>Namaste from JSX 🚀</h1>)
+
+// After transpilation(converting) the jsxHeading will become something like this, which JS engine understand, the above syntax is not understand by JS Engine so it is converted by parcel or vite or any other with the help of babel(a JS package) before it goes to JS Engine.
+
+// const jsxHeading = React.createElement("h1", {id: "heading", className: "head"}, "Namaste from JSX🚀")
+
+console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(jsxHeading);
