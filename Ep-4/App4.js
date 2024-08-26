20f1a13110d88131dfe5bf9e5012610ed5505d0f
DeepankarMalakar <deepankarmalakar007@gmail.com>
2024-08-26 22:19:49 +0530

    Updated

--- Ep-4/App4.js
+++ Ep-4/App4.js
@@ -42,31 +42,49 @@ const Header = () => {
 // - Time to Deliver
 
 const RestaurantCard = (props) => {
-    console.log(props);
+    const { resData } = props;
     return (
         <div className="res-card">
-        <img className='res-img' src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/xph0vf5pvbxjf8xchwjk" alt="" />
-            <h3>{props.resName}</h3>
-            <h5>{props.cuisine}</h5>
-            <span>{props.rating}</span>
+            <img className='res-img' src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/xph0vf5pvbxjf8xchwjk" alt="" />
+            <h3>{resData.name}</h3>
+            <h5>{resData.cuisines}</h5>
+            <span>{resData.areaName}</span>
+            <span>{resData.avgRating}⭐</span>
         </div>
     )
+}
+
+const resObj = {
+    "id": "303066",
+    "name": "Haji Biryani Main Branch",
+    "cloudinaryImageId": "FOOD_CATALOG/IMAGES/CMS/2024/3/22/2cf5b8ff-c561-46b0-9f09-032f8c244c25_9d9f0bb3-a1d4-4a8a-9c0a-72055c6d0007.jpg",
+    "locality": "Nivedita Road",
+    "areaName": "Pradhan Nagar",
+    "costForTwo": "₹200 for two",
+    "cuisines": [
+        "Biryani ",
+        "rolls ",
+        "Mughlai ",
+        "Chinese "
+    ],
+    "avgRating": 4.1,
+    "parentId": "486390",
+    "avgRatingString": "4.1"
 }
 
 const Body = () => {
     return (
         <div className="body">
             <div className="search-bar">
-                <input type="text" placeholder='Search your Craving'/>
+                <input type="text" placeholder='Search your Craving' />
                 <button>Search</button>
             </div>
             <div className="res-container">
-                <RestaurantCard resName="Gastronomical Odyssey" cuisine="Mutton Kosha, Chicken Kosha, Fish Kosha" rating="⭐⭐⭐⭐" />
-                <RestaurantCard resName="The Fork in the Road" cuisine="North India, South India, Chinese" rating="⭐⭐⭐⭐"/>
-                <RestaurantCard resName="The Flavorful Frontier" cuisine="Icecreams, Cakes, Desserts" rating="⭐⭐⭐⭐"/>
-                <RestaurantCard resName="The Cosmic Cafe" cuisine="Coffees, Tea, Cakes" rating="⭐⭐⭐⭐"/>
-                <RestaurantCard resName="Pizza My Heart" cuisine="Pizza, Sandwiches, Burger" rating="⭐⭐⭐"/>
-                <RestaurantCard resName="KFC" cuisine="Fry Chicken, Chicken nuggets, Burger" rating="⭐⭐⭐⭐"/>
+                <RestaurantCard resData={resObj} />
+                {/* <RestaurantCard resName="The Fork in the Road" cuisine="North India, South India, Chinese" rating="⭐⭐⭐⭐" />
+                <RestaurantCard resName="The Flavorful Frontier" cuisine="Icecreams, Cakes, Desserts" rating="⭐⭐⭐⭐" />
+                <RestaurantCard resName="The Cosmic Cafe" cuisine="Coffees, Tea, Cakes" rating="⭐⭐⭐⭐" />
+                <RestaurantCard resName="Pizza My Heart" cuisine="Pizza, Sandwiches, Burger" rating="⭐⭐⭐" /> */}
             </div>
         </div>
     )
