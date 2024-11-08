78220ddfe6562716e36dd2e6c4c0f98644865cd9
DeepankarMalakar <deepankarmalakar007@gmail.com>
2024-11-08 16:56:56 +0530

    Updated and added new lines

--- Ep-5-Hooks/src/components/Constants.js
+++ Ep-5-Hooks/src/components/Constants.js
@@ -1,1 +1,3 @@
 export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9352403&lng=77.624532&restaurantId="
+
+export const IMAGE_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/"
--- Ep-5-Hooks/src/components/RestaurantMenu.js
+++ Ep-5-Hooks/src/components/RestaurantMenu.js
@@ -2,33 +2,32 @@ import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
 import { MENU_API } from "./Constants";
+import { IMAGE_URL } from "./Constants";
+import Accordion from 'react-bootstrap/Accordion';
 
 const RestaurantMenu = () => {
     const [resInfo, setResInfo] = useState(null);
     const { resId } = useParams();
-    console.log(resId)
 
     useEffect(() => {
         fetchMenu();
-    }, [])
+    }, []);
 
     const fetchMenu = async () => {
-        const data = await fetch(
-            MENU_API + resId
-        );
-
+        const data = await fetch(MENU_API + resId);
         const json = await data.json();
-        console.log(json);
         setResInfo(json.data);
-    }
+    };
 
-    if (resInfo === null) return <Shimmer />
-
+    if (resInfo === null) return <Shimmer />;
 
     const { name, totalRatingsString, cuisines, costForTwoMessage, avgRatingString, areaName, city, sla } = resInfo?.cards[2]?.card?.card?.info;
-
     const { itemCards } = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
-    console.log(itemCards);
+    console.log(itemCards)
+
+    // Filter items into Veg and Non-Veg categories
+    const vegItems = itemCards.filter(item => item?.card?.info?.itemAttribute?.vegClassifier === 'VEG');
+    const nonVegItems = itemCards.filter(item => item?.card?.info?.itemAttribute?.vegClassifier === 'NONVEG');
 
     return (
         <>
@@ -45,15 +44,60 @@ const RestaurantMenu = () => {
                     <li><span>{sla.minDeliveryTime} - {sla.maxDeliveryTime} mins</span></li>
                 </div>
             </div>
-            <div className="menu">
-                <p>Menu</p>
-                <ul>{itemCards.map(item => <li key={item.card.info.id}>{item.card.info.name} - {"Rs."}{item.card.info.price / 100 || item.card.info.defaultPrice / 100}</li>)}</ul>
+            <div className="menu-header">
+                <h2>Menu</h2>
             </div>
-            <div>
-                <button className="btn btn-warning">Hello</button>
-            </div>
+
+            <Accordion>
+                {/* Veg Accordion Section */}
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header>Veg</Accordion.Header>
+                    <Accordion.Body>
+                        {/* <ul>
+                                {vegItems.map(item => (
+                                    <li key={item.card.info.id}>
+                                        {item.card.info.name} - Rs. {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
+                                    </li>
+                                ))}
+                            </ul> */}
+                        <div className="items">
+                            {vegItems.map(item => (
+                                <div key={item.card.info.id} className="eachItem">
+                                    <a href="#">{item.card.info.name}</a> - Rs. {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
+                                    <ul>
+                                        <li className="eachDescription">{item.card.info.description}</li>
+                                    </ul>
+                                    <div className="itemImageContainer">
+                                        <img src={IMAGE_URL + item.card.info.imageId} alt="" />
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </Accordion.Body>
+                </Accordion.Item>
+
+                {/* Non-Veg Accordion Section */}
+                <Accordion.Item eventKey="1">
+                    <Accordion.Header>Non-Veg</Accordion.Header>
+                    <Accordion.Body>
+                        <div className="items">
+                            {nonVegItems.map(item => (
+                                <div key={item.card.info.id} className="eachItem">
+                                    <a href="#">{item.card.info.name}</a> - Rs. {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
+                                    <ul>
+                                        <li className="eachDescription">{item.card.info.description}</li>
+                                    </ul>
+                                    <div className="itemImageContainer">
+                                        <img src={IMAGE_URL + item.card.info.imageId} alt="" />
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
         </>
     );
-}
+};
 
 export default RestaurantMenu;
