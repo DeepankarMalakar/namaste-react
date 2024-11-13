e15c54a2b6d57b873dec7561b73b5279233f6a34
DeepankarMalakar <deepankarmalakar007@gmail.com>
2024-11-13 17:45:08 +0530

    Updated and added new files

--- Ep-5-Hooks/src/components/AboutUs.js
+++ Ep-5-Hooks/src/components/AboutUs.js
@@ -1,6 +1,18 @@
+import UserClass from "./UserClass";
 const AboutUs = () => {
     return (
-        <div><h1>Hello , Welcome to About Page</h1></div>
+        <>
+            <div className="about-us">
+                <div className="about-profile">
+                    <UserClass name = "Lorem Ipsum" profile = "Profile" />
+                </div>
+                <div className="about-description">
+                    <h1><span>Food</span> App</h1>
+                    <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat doloribus quasi, neque assumenda animi dolores tempore laboriosam optio aspernatur itaque. Facere sapiente eius sed accusantium quia, deserunt modi provident? Voluptatibus sint quas, quo amet nesciunt quia magni numquam nihil tempora asperiores veritatis, eligendi maxime totam sequi repellat officiis saepe voluptas hic voluptatem iste. Quam ad, error nam, consequatur iusto consectetur aliquam quidem natus earum exercitationem eius cum fugit.</span>
+                </div>
+
+            </div>
+        </>
     )
 }
 
---
+++ Ep-5-Hooks/src/components/UserClass.js
@@ -0,0 +1,48 @@
+// Class based component for about us page 
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+const img = "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+
+class UserClass extends React.Component {
+    constructor(props) {
+        super(props);
+        // console.log(props)
+        // use State hook or variables are not used in class
+        // const [count] = useState(0)
+        // const [count2] = useState(2)
+        // Instead this was the way to use:
+        // this.state = {
+        //     count: 0,
+        //     count2: 2,
+        // };
+    }
+    render() {
+        const {name, profile} = this.props
+        // const {count, count2} = this.state;
+        return (
+            <Card style={{ width: '18rem', marginLeft: '1rem', marginTop: '1rem'}}>
+                <Card.Img variant="top" src= {img}/>
+                <Card.Body>
+                    <Card.Title>{name} (CEO)</Card.Title>
+
+                    {/* <span>{this.state.count}</span> */}
+                    {/* <span>{count}</span> */}
+                    {/* <span>{count2}</span> */}
+
+                    {/* <button onClick={() => this.setState({
+                        count: this.state.count + 1,
+                    })}>Increment</button> */}
+
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">{profile}</Button>
+                </Card.Body>
+            </Card>
+        )
+    }
+}
+
+export default UserClass
